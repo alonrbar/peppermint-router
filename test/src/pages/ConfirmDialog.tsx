e46ce308fbe9087c2e0191c3732ec884fe82734b
Alon Bar <alon.r.bar@gmail.com>
2019-07-26 17:44:29 +0300

    test exit prompt

--- test/src/pages/ConfirmDialog.tsx
+++ test/src/pages/ConfirmDialog.tsx
@@ -21,26 +21,32 @@ export interface ConfirmDialogProps {
     enabled: boolean;
 }
 
-export const ConfirmDialog: React.SFC<ConfirmDialogProps> = (props) => (
-    <PromptNavigation enabled={props.enabled}>
-        {({ isNavigating, confirm, cancel }) => (
-            isNavigating && (
-                <div style={styles.confirmDialog}>
+export const ConfirmDialog: React.SFC<ConfirmDialogProps> = (props) => {
+    const message = 'Are you sure you want to leave?';
+    return (
+        <PromptNavigation
+            enabled={props.enabled}
+            exitPrompt={message}
+        >
+            {({ isNavigating, confirm, cancel }) => (
+                isNavigating && (
+                    <div style={styles.confirmDialog}>
 
-                    <span style={styles.confirmDialogText}>
-                        Are you sure you want to leave?
-                    </span>
+                        <span style={styles.confirmDialogText}>
+                            {message}
+                        </span>
 
-                    <button style={styles.confirmDialogButton} onClick={confirm}>
-                        Confirm
-                    </button>
+                        <button style={styles.confirmDialogButton} onClick={confirm}>
+                            Confirm
+                        </button>
 
-                    <button style={styles.confirmDialogButton} onClick={cancel}>
-                        Cancel
-                    </button>
+                        <button style={styles.confirmDialogButton} onClick={cancel}>
+                            Cancel
+                        </button>
 
-                </div>
-            )
-        )}
-    </PromptNavigation>
-);
+                    </div>
+                )
+            )}
+        </PromptNavigation>
+    );
+};
